5926ce8aefafb30700eb01b612921aa4b0a314af
Jason Bock <jason.r.bock@outlook.com>
2023-04-14 15:48:40 -0500

    Started adding examples

---
+++ README.md
@@ -0,0 +1,2 @@
+# "What's New in C# 12" Demo Code
+This repo contains the demo code used for my "What's New in C# 12" talk.
---
+++ WhatsNewInCSharp12/Program.cs
@@ -0,0 +1,36 @@
+﻿using Person = (string name, uint age);
+
+#pragma warning disable CA1852 // Seal internal types
+
+DemonstrateAliasesForAnyType();
+
+static void DemonstrateAliasesForAnyType()
+{
+	Console.WriteLine(nameof(DemonstrateAliasesForAnyType));
+	Console.WriteLine();
+
+	static Person GetPerson() => ("Jason", 30);
+
+	var person = GetPerson();
+
+	Console.WriteLine(person);
+}
+
+//DemonstrateDefaultParameterValuesForLambda();
+
+static void DemonstrateDefaultParameterValuesForLambda()
+{
+	Console.WriteLine(nameof(DemonstrateDefaultParameterValuesForLambda));
+	Console.WriteLine();
+
+	var personGenerator = (string name = "Jason", uint age = 30) => new Person(name, age);
+
+	var jasonPerson = personGenerator();
+	Console.WriteLine(jasonPerson);
+
+	var janePerson = personGenerator("Jane");
+	Console.WriteLine(janePerson);
+
+	var johnPerson = personGenerator("John", 22);
+	Console.WriteLine(johnPerson);
+}
